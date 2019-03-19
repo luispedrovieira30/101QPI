dc740477d9ac02bc1432b48c933a4ce87a5e7e1d
Massas <48384492+30Massas@users.noreply.github.com>
2019-03-19 23:49:29 +0000

    Update q012.c

--- q012.c
+++ q012.c
@@ -2,3 +2,16 @@ int isVowel(char s){
     if(s=='a' || s=='e' || s=='i' || s=='o' || s=='u' || s== 'A' || s== 'E' || s=='I' || s=='O' || s=='U') return 1;
     else return 0;
 }
+
+void strnoV (char s[]){
+    int i, j= strlen(s), n;
+    char v[j];
+    strcpy(v,s);
+    for (i=n=0; v[i]|= '\0'; i++){
+        if(!(isVowel(s[i]))){
+            s[n]=v[i];
+            n++;
+        }
+    }
+    s[n]='\0';
+}
