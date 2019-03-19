6ff317a2699e4279d4c56353a1985885142fcd65
Massas <48384492+30Massas@users.noreply.github.com>
2019-03-19 23:48:18 +0000

    Create q010.c

---
+++ q010.c
@@ -0,0 +1,10 @@
+char *mystrstr(char s1[], char s2[]) {
+    int i,j=0, pos=0;
+    if (s2[0] == '\0') return s1;
+    for(i=0; s1[i] != '\0'; i++){
+        if (s1[i] == s2[0]) pos=i;
+        if (s1[i] == s2[j]) j++;
+        if (s2[j] == '\0') return s1+pos;
+    }
+    return NULL;
+}
