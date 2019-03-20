f7957d44b30a770e26fe53f76b33e7bab72ca5c7
Massas <48384492+30Massas@users.noreply.github.com>
2019-03-20 00:02:38 +0000

    Create q025.c

---
+++ q025.c
@@ -0,0 +1,14 @@
+int limpaEspaços(char s[]){
+    int i,j;
+    for (i=0; s[i] != '\0'; i++){
+        if(isspace(s[i]) && (s[i+1] == s[i])){
+            for (j=i; s[j] != '\0'; j++){
+                s[j] = s[j+1];
+            }
+            s[j]= '\0';
+            i--;
+        }
+    }
+    int n = strlen(s);
+    return n;
+}
